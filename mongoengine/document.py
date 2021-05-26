187b22377180b9f4af07ba11b585bfa89de7c2ed
yguo <yiguo@wish.com>
2021-05-26 23:44:19 +0000

    update read preference

--- mongoengine/document.py
+++ mongoengine/document.py
@@ -1021,8 +1021,7 @@ class Document(BaseDocument):
                 return {'result': results}
         cls._transformKwargs(kwargs)
         cur = cls._pymongo(
-                # TODO: yiguo - it's causing stuck in stage, nor sure it causes same issue in prod or not
-                # read_preference=pymongo.read_preferences.ReadPreference.SECONDARY
+                read_preference=pymongo.read_preferences.ReadPreference.SECONDARY_PREFERRED
             ).aggregate(
                 pipeline,
                 **kwargs
